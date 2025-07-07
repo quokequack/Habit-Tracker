6b98eedf837f81a892ede862b6aded800c472845
Alaquoke <alaquoke@gmail.com>
2025-07-07 18:30:38 -0300

    config(inicio): configurando docker

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
+import { router } from './router/index.ts';
 
 
 const app = createApp(App);
-app.use(vuetify).mount("#app");
+app.use(router).use(vuetify).mount("#app");
